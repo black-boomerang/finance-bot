741072568b320e828b5b1480f3d045224198935d
Alexey <alejo2000@yandex.ru>
2021-02-03 20:07:55 +0300

    update

--- bot/finance_bot.py
+++ bot/finance_bot.py
@@ -28,8 +28,8 @@ class FinanceBot(telebot.TeleBot):
             callback_data='help')
 
         self.thread = ScheduleThread(self.send_recommendations, 'cron',
-                                     day_of_week='mon-fri', hour=17,
-                                     minute=45)
+                                     day_of_week='mon-fri', hour=20,
+                                     minute=12)
         self.thread.start()
 
     @staticmethod
